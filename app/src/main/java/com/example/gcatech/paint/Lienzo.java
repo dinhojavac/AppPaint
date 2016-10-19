b3f5137437bc941840b734f55f33e1a9d25b5488
dinhojavac <anderteinco@gmail.com>
2016-10-19 16:44:46 -0500

    App Paint Version 1

---
+++ app/src/main/java/com/example/gcatech/paint/Lienzo.java
@@ -0,0 +1,147 @@
+package com.example.gcatech.paint;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by GCATech on 13/10/2016.
+ */
+
+public class Lienzo extends View {
+    private float startX;
+    private float startY;
+    private float endX;
+    private float endY;
+    //la clase path es la que va ir guardando el trazo por el cual voy a ir pintando
+    private Path drawPath;
+    //es un pincel, Paint de dibujar y canvas
+    private Paint drawPaint,canvasPaint;
+    //color inicial
+    private int paintColor=0xffff0000;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas para guardar, el tipo de archivo a construir
+    private Bitmap canvasBitmap;
+
+    public Lienzo(Context context) {
+        super(context);
+    }
+
+    public Lienzo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    //métodos
+    private void setupDrawing(){
+        //configuración del área sobre la que pintar
+        drawPath=new Path();
+        drawPaint=new Paint();
+        //color inicial
+        drawPaint.setColor(paintColor);
+        //el tema de los trazos sean mas suaves con los strokes
+        drawPaint.setAntiAlias(true);
+        //es el ancho
+        //drawPaint.setStrokeWidth(20);
+        //pintar solamente trazos
+        drawPaint.setStyle(Paint.Style.STROKE);
+        //pintar redondeado
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        //es el objeto, dither flag permite pintar mas difuminado
+        canvasPaint=new Paint(Paint.DITHER_FLAG);
+
+    }
+
+    //tamano asignado a la vista- son funciones que vienen
+    //es una vista que cuando la creas pone las caracteristicas
+    @Override
+    protected void onSizeChanged(int w,int h,int oldw,int oldh){
+        super.onSizeChanged(w,h,oldw,oldh);
+        //objetos que va obtener
+        canvasBitmap=Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+        //se obtiene un nuevo canvas dentro para pintar sobre el
+        drawCanvas=new Canvas(canvasBitmap);
+    }
+
+    //Pinta la vista. Será llamado desde el OnTouchEvent
+    //se repinta sobre el, se repinta el canvas
+    @Override
+    protected void onDraw(Canvas canvas){
+        //es un dibujo en memoria con un formato, que trabajara con canvasPaint
+        //mapa de bits
+        canvas.drawBitmap(canvasBitmap,0,0,canvasPaint);
+        canvas.drawPath(drawPath,drawPaint);
+        canvas.drawLine(startX, startY, endX, endY, drawPaint);
+    }
+
+    //funcion Touch-registra los touch de usuario
+    //va controlar los eventos del usuario
+
+    public boolean onTouchEvent(MotionEvent event){
+        /*float touchX=event.getX();
+        float touchY=event.getY();
+        float endX=event.getX();
+        float endY=event.getY();*/
+
+        switch (event.getAction()){
+            //cuando se pulsa con el dedo
+            case MotionEvent.ACTION_DOWN:
+                //drawPath.moveTo(touchX,touchY);
+                startX = event.getX();
+                startY = event.getY();
+                invalidate();
+                break;
+            //moviendo el dedo
+            case MotionEvent.ACTION_MOVE:
+                //construye una linea por el movimiento del dedo
+                //drawPath.lineTo(endX,endY);
+                endX = event.getX();
+                endY = event.getY();
+                invalidate();
+                break;
+            //cuando se levanta
+            case MotionEvent.ACTION_UP:
+                //para al final hacer el ultimo trazito de la linea
+                //drawPath.lineTo(endX,endY);
+                //y para pasarle al canvas para que pueda pintar tdo el seguimiento del dedo
+                endX = event.getX();
+                endY = event.getY();
+                drawCanvas.drawLine(startX, startY, endX, endY, drawPaint);
+                //drawCanvas.drawPath(drawPath,drawPaint);
+                //drawPath.reset();
+                invalidate();
+                break;
+            default:
+                return false;
+        }
+        //repintar
+        //es llamar a repintar el canvas, cada vez que se esta haciendo en touch event se esta entrando o llamando a repintar el canvas
+        //sin parar, para que vaya actualizandolo tdo
+        //esta función invalidate nos va permitir sobreescribir el  metodo onDraw() que hay que sobreescribirlo en todos los view
+        invalidate();
+        return true;
+    }
+
+    //funcion pintar color
+    //actualiza color
+
+    public void setColor(String newColor){
+        invalidate();
+        //coloca el nuevo color
+        paintColor= Color.parseColor(newColor);
+        drawPaint.setColor(paintColor);
+    }
+    public Lienzo(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/gcatech/paint/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.gcatech.paint;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton colorNegro,colorBlanco,colorAzul,colorRojo,colorVerde;
+    //enlazar la clase lienzo
+    Lienzo lienzo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar=(Toolbar)findViewById(R.id.Toolbar);
+        //setSupportActionBar(toolbar);
+
+        colorNegro=(ImageButton)findViewById(R.id.ColorNegro);
+        colorBlanco=(ImageButton)findViewById(R.id.ColorBlanco);
+        colorAzul=(ImageButton)findViewById(R.id.ColorAzul);
+        colorRojo=(ImageButton)findViewById(R.id.ColorRojo);
+        colorVerde=(ImageButton)findViewById(R.id.ColorVerde);
+
+        //llamada a la escucha
+        colorNegro.setOnClickListener(this);
+        colorBlanco.setOnClickListener(this);
+        colorAzul.setOnClickListener(this);
+        colorRojo.setOnClickListener(this);
+        colorVerde.setOnClickListener(this);
+
+        lienzo=(Lienzo)findViewById(R.id.lienzo);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String color=null;
+
+        switch (v.getId()){
+            case R.id.ColorNegro:
+                //coloca el color que corresponde
+                //llama a getTag() que es el trae el color y lo convierte a string por medio de toString()
+                color=v.getTag().toString();
+                lienzo.setColor(color);
+                break;
+
+            case R.id.ColorBlanco:
+                color=v.getTag().toString();
+                lienzo.setColor(color);
+                break;
+            case R.id.ColorAzul:
+                color=v.getTag().toString();
+                lienzo.setColor(color);
+                break;
+            case R.id.ColorRojo:
+                color=v.getTag().toString();
+                lienzo.setColor(color);
+                break;
+            case R.id.ColorVerde:
+                color=v.getTag().toString();
+                lienzo.setColor(color);
+                break;
+
+            default:
+
+                break;
+
+        }
+    }
+}
